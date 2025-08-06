be22c6d49bd277a956a0ec26fbddbe4d1d0f0106
Rakeb Yared <rakebyaredt@gmail.com>
2025-08-06 19:22:53 +0300

    managerDashboard and manageApplicant created

--- application_platform/src/app/page.tsx
+++ application_platform/src/app/page.tsx
@@ -1,10 +1,11 @@
 import BodyOne from "../components/BodyOne";
 import Link from "next/link";
+import ManagerDashboard from "./managerDashboard/page";
 
 export default function Home() {
   return (
     <>
-      <BodyOne/>
+      <BodyOne/>   
     </>
   );
 }
